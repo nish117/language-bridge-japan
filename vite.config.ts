07866d201de52505c37d4a4c0c7ad037ca6e46db
nishant bajracharya <bajra.nish@gmail.com>
2025-03-13 23:07:28 +0545

    FIXED codes for live upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/language-bridge-japan/", // Add base path for GitHub Pages
+  // base: "/language-bridge-japan/", // Add base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
