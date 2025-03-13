5315838e9c0ab243ec482a888a88114049252cf0
nishant bajracharya <bajra.nish@gmail.com>
2025-03-13 20:21:26 +0545

    FIxed url link

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/language-bridge-japan",
+  base: "/language-bridge-japan/",
   server: {
     host: "::",
     port: 8080,
